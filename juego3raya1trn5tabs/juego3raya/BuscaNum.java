b6b5744915c3c68220c923158cfe93b997eda0d9
refnandos <145782577+refnandos@users.noreply.github.com>
2024-04-30 02:00:40 +0200

    V2 DE TRES EN RAYA CORRECCION DE ERRORES
    se han corregido:
    -los turnos ahora van por tablero y pasan de uno a otro
    -mejoras en errores con el Scanner(saltaba algunas lineas de lectura)
    -correccion en algunas lecturas aceptando caracteres en lugar de numeros(aun hay algunos que no logre evitar)

---
+++ juego3raya1trn5tabs/juego3raya/BuscaNum.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego3raya;
+
+/**
+ *
+ * @author Usuario
+ */
+public class BuscaNum extends JuegoMadre {
+
+
+    @Override
+    public void juego() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isTerminado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
